cc6a3528dd99877ef3e4bfabf42a0d26b52274fe
matheuskaully <kaully.santos@gmail.com>
2023-01-17 16:32:25 -0300

    server

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+app.register(cors)
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 3333,
+})
+.then(() => {
+  console.log('Running in port 3333')
+})
+
